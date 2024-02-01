d5c7f9d7b739de1c5922595d3a5f66d8b5713a2b
marcopezzote <pezzote@outlook.com>
2024-02-01 18:09:06 -0300

    Solução implementada

--- 01/index.js
+++ 01/index.js
@@ -3,12 +3,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const jogadores = ['José', 'Maria', 'João', 'Marcos', 'Fernanda'];
 
+let index = 0;
 
+app.get('/', (req, res) => {
 
+    res.send(jogadores[index]);
 
+    index = (index + 1) % jogadores.length;
 
-
+});
 
 app.listen(port, () => {
     console.log(`> > > Servidor inicializado na porta ${port}`);
